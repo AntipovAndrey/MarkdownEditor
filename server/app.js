f937242cd8bdb28b3ae6da4289ebccb36238469c
AntipovAndrey <antipov11.11@gmail.com>
2019-04-27 13:07:36 +0300

    [server] crud endpoints for document

--- server/app.js
+++ server/app.js
@@ -1,5 +1,15 @@
+const createError = require('http-errors');
 const express = require('express');
 const logger = require('morgan');
+
+const mongoose = require('mongoose');
+
+mongoose.plugin(schema => {
+  schema.statics.isObjectId = id => id && /^[0-9a-fA-F]{24}$/.test(id);
+});
+
+mongoose.connect('mongodb://localhost/markdown', {useNewUrlParser: true});
+mongoose.set('useFindAndModify', false);
 
 const indexRouter = require('./routes/index');
 
@@ -7,8 +17,22 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 app.use('/', indexRouter);
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+  const status = !err.status ? 500 : err.status;
+  res.status(status)
+    .json({
+      message: err.message,
+      error: req.app.get('env') === 'development' ? err : {},
+      status: status
+    });
+});
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,45 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({'d':10});
+const documents = require('../service/documents');
+
+router.post('/', async (req, res, next) => {
+  try {
+    await documents.create(req.body);
+    res.sendStatus(201);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get('/', async (req, res) => {
+  res.json(await documents.getPreviews());
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    res.json(await documents.findById(req.params.id))
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    await documents.deleteDocument(req.params.id);
+    res.sendStatus(200)
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    await documents.replaceDocument(req.params.id, req.body);
+    res.sendStatus(200)
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
